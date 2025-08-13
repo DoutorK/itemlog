ce7ff4315156a7bfa1c0191d1abdb6a562c9c1be
DoutorK <giovannylucas28@gmail.com>
2025-08-13 17:43:20 -0300

    refactor(department): remove unused items relationship method

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -6,12 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Department extends Model
 {
-    //campos que podem ser preenchidos em massa
-    protected $fillable = ['name']; 
 
-    public function items()
-    {
-        //um departamento tem muitos itens
-        return $this->hasMany(Item::class); 
-    }
+    protected $fillable = ['name']; 
+    
 }

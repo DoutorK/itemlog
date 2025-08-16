e7cccf7b89e55d2deda482a2328fca312aab08bc
DoutorK <giovannylucas28@gmail.com>
2025-08-16 14:16:49 -0300

    refactor(app.ts): remove Items component from app initialization

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,16 +1,13 @@
 import { createApp } from "vue";
 import Departments from "../js/components/Departments.vue";
-import Items from "./components/Items.vue";
 import "../css/app.css";
 
 const app = createApp({
     components: {
         Departments,
-        Items
     },
     template: `
       <Departments />
-      <Items />
     `
 });
 

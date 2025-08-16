5fd3498b33d0af83a06d290a212047c9704e9c2b
DoutorK <giovannylucas28@gmail.com>
2025-08-16 16:42:40 -0300

    feat(Home.vue): add new page component for displaying departments

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home-container">
+    <h1 class="page-title">Departamentos</h1>
+    <DepartmentList />
+  </div>
+</template>
+
+<script setup lang="ts">
+
+import DepartmentList from "../components/DepartmentList.vue";
+import "../styles/home.css";
+
+</script>

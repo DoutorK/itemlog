2e959a4401c396f0dfdaa254337106a1794d81b7
DoutorK <giovannylucas28@gmail.com>
2025-08-16 14:39:20 -0300

    feat(api.php): add custom routes for department-specific item management

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,7 @@ use App\Http\Controllers\Api\ItemController;
 
 Route::apiResource('departments', DepartmentController::class);
 Route::apiResource('items', ItemController::class)->shallow();
+
+// Rotas customizadas para itens de um departamento
+Route::get('departments/{department}/items', [App\Http\Controllers\Api\ItemController::class, 'itemsByDepartment']);
+Route::post('departments/{department}/items', [App\Http\Controllers\Api\ItemController::class, 'storeForDepartment']);
